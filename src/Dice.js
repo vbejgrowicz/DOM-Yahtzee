730a41a82cfc721101750af1418ea2943bc91d8e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-10-30 12:16:46 -0400

    Fix: Roll dice to only occur on not held dice

--- src/Dice.js
+++ src/Dice.js
@@ -29,11 +29,15 @@ export default class Dice {
     this.display(diceArr, 'show');
     const newArr = [];
     diceArr.forEach((dice, idx) => {
-      const roll = Math.round((Math.random() * 5) + 1);
       const diceNum = idx + 1;
       const DOMselector = document.getElementById(`dice${diceNum}`);
-      DOMselector.src = `dice/dice-${roll}.png`;
-      newArr.push(roll);
+      if (!DOMselector.classList.contains('hold')) {
+        const roll = Math.round((Math.random() * 5) + 1);
+        DOMselector.src = `dice/dice-${roll}.png`;
+        newArr.push(roll);
+      } else {
+        newArr.push(dice);
+      }
     });
     return newArr;
   }
