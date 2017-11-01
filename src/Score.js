a11cceb8285d950d79f213f1e4110fb51bfd2fa7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-01 12:01:39 -0400

    Feature: Add scoring functions for three, four, and five of a kind

--- src/Score.js
+++ src/Score.js
@@ -8,13 +8,26 @@ export default class Score {
     const diceArr = diceObj.map(die => die.value);
     let score;
     const count = (num) => {
-      let total = 0;
+      score = 0;
       for (let i = 0; i < diceArr.length; i += 1) {
         if (diceArr[i] === num) {
-          total += num;
+          score += num;
         }
       }
-      return total;
+      return score;
+    };
+    const countInstance = () => {
+      const result = diceArr.reduce((acc, val) => acc.set(val, 1 + (acc.get(val) || 0)), new Map());
+      return result;
+    };
+    const checkInstances = (limit, scoring) => {
+      score = 0;
+      countInstance().forEach((value, key) => {
+        if (value >= limit) {
+          score = scoring || (key * value);
+        }
+      });
+      return score;
     };
     switch (this.category) {
       case 'one':
@@ -29,6 +42,12 @@ export default class Score {
         return count(5);
       case 'six':
         return count(6);
+      case 'threekind':
+        return checkInstances(3);
+      case 'fourkind':
+        return checkInstances(4);
+      case 'yahtzee':
+        return checkInstances(5, 50);
       default:
         console.log('else');
         return score;
