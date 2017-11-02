640787e4cf97b2a2c920e7597db92699949238c5
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-02 14:03:26 -0400

    Refactor: Refactor scoring functions

--- src/Score.js
+++ src/Score.js
@@ -5,79 +5,53 @@ export default class Score {
     this.value = this.calculate(diceObj);
   }
   calculate(diceObj) {
-    let num;
     let score = 0;
+    let numOf;
+    const values = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+    };
     const diceArr = diceObj.map(die => die.value);
     const sumDice = diceArr.reduce((a, b) => a + b, 0);
+    const isUnique = [...new Set(diceArr)];
 
     const count = curNum => diceArr.filter(die => die === curNum);
 
-    const counted = {
-      1: count(1).length,
-      2: count(2).length,
-      3: count(3).length,
-      4: count(4).length,
-      5: count(5).length,
-      6: count(6).length,
-    };
-
-    const forCounted = (limit) => {
-      let result = false;
-      Object.entries(counted).forEach((current) => {
-        const val = current[1];
-        if (val >= limit) {
-          result = true;
-        }
-      });
-      return result;
-    };
-
-    const fullhouseTest = () => {
-      Object.entries(counted).forEach((current) => {
-        if (current[1] === 3) {
-          Object.entries(counted).forEach((cur) => {
-            if (cur[1] === 2) {
-              score = 25;
-            }
-          });
+    const checkMultiple = (limit) => {
+      isUnique.forEach((num) => {
+        numOf = count(num).length;
+        if (numOf >= limit) {
+          score = sumDice;
         }
       });
       return score;
     };
 
     switch (this.category) {
-      case 'one':
-        num = 1;
-        return counted[num] * num;
-      case 'two':
-        num = 2;
-        return counted[num] * num;
-      case 'three':
-        num = 3;
-        return counted[num] * num;
-      case 'four':
-        num = 4;
-        return counted[num] * num;
-      case 'five':
-        num = 5;
-        return counted[num] * num;
-      case 'six':
-        num = 6;
-        return counted[num] * num;
       case 'threekind':
-        if (forCounted(3)) {
-          score = sumDice;
+        if (isUnique.length <= 3) {
+          score = checkMultiple(3);
         }
         return score;
       case 'fourkind':
-        if (forCounted(4)) {
-          score = sumDice;
+        if (isUnique.length <= 2) {
+          score = checkMultiple(4);
         }
         return score;
       case 'fullhouse':
-        return fullhouseTest();
+        if (isUnique.length === 2) {
+          numOf = count(isUnique[0]).length;
+          if (numOf === 2 || numOf === 3) {
+            score = 25;
+          }
+        }
+        return score;
       case 'yahtzee':
-        if (forCounted(5)) {
+        if (isUnique.length === 1) {
           score = 50;
         }
         return score;
@@ -85,6 +59,7 @@ export default class Score {
         score = sumDice;
         return score;
       default:
+        score = count(values[this.category]).length * values[this.category];
         return score;
     }
   }
